52864b25ca5ffeff0b09ff9249a20bf1169cf944
maini <philip.mattsson@iths.se>
2021-02-19 20:49:11 +0100

    Version 0.1

---
+++ src/main/java/se/iths/lab2webservice/Lab2webserviceApplication.java
@@ -0,0 +1,13 @@
+package se.iths.lab2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2webserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2webserviceApplication.class, args);
+    }
+
+}
