a5d7b204665464b15407be4eecf811050cf8e863
maini <philip.mattsson@iths.se>
2021-03-05 16:21:39 +0100

    Version 1.10

--- src/test/java/se/iths/lab2webservice/Lab2webserviceApplicationTests.java
+++ src/test/java/se/iths/lab2webservice/Lab2webserviceApplicationTests.java
@@ -1,13 +1,11 @@
 package se.iths.lab2webservice;
 
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import se.iths.lab2webservice.dtos.BookDto;
 import org.springframework.http.HttpHeaders;
 
